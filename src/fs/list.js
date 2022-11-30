640cfd696e58c9a11f22b0b2ac7b74b000215fce
Anton Trafimovich <antont@compatibl.com>
2022-11-30 11:04:54 +0100

    feat: add list.js implementation.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir } from "fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const dirPath = path.resolve(__dirname, "files");
+
+  try {
+    const files = await readdir(dirPath);
+    console.log(files);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await list();
