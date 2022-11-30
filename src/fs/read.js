d828d27bff48215e41ffc7be888b9714cac50f70
Anton Trafimovich <antont@compatibl.com>
2022-11-30 11:09:30 +0100

    feat: add read.js implementation.

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToReadPath = path.resolve(__dirname, "files", "fileToRead.txt");
+
+  try {
+    const content = await readFile(fileToReadPath, { encoding: "utf-8" });
+    console.log(content);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await read();
