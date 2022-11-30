1b6682c9ab4c245d4e9d6da2467e25ee3a27f913
Anton Trafimovich <antont@compatibl.com>
2022-11-30 10:48:16 +0100

    feat: add rename.js implementation.

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import { rename as fsRename, open } from "fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const from = path.resolve(__dirname, "files", "wrongFilename.txt");
+  const to = path.resolve(__dirname, "files", "properFilename.md");
+
+  let file;
+
+  try {
+    file = await open(to);
+  } catch {}
+
+  if (file) {
+    await file.close();
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fsRename(from, to);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await rename();
