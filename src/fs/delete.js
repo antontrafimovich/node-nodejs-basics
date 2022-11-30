74daba6d0519a9fd9a39ad882a5a66d649a204c0
Anton Trafimovich <antont@compatibl.com>
2022-11-30 10:54:20 +0100

    feat: add delete.js implementation.

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from "fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToRemove = path.resolve(__dirname, "files", "fileToRemove.txt");
+
+  try {
+    await rm(fileToRemove);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await remove();
