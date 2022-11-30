cd07549675437916b478e24c3e3b4a081d3d93ce
Anton Trafimovich <antont@compatibl.com>
2022-11-30 10:43:55 +0100

    feat: add create.js implementation.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import { appendFile, open } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = "fresh.txt";
+  const content = "I am fresh and young";
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.resolve(__dirname, "files", fileName);
+
+  let file;
+
+  try {
+    file = await open(filePath);
+  } catch {}
+
+  if (file) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await appendFile(filePath, content);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
