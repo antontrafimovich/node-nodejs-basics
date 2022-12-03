157e08ff3e20f0ce177dfaf69e62fb94f0f265c4
Anton Trafimovich <antont@compatibl.com>
2022-12-03 15:08:54 +0100

    feat: add stream write.js implementation.

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+
+import { fileURLToPath } from "node:url";
+import path from "path";
+import { stdin } from "node:process";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToWritePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+
+  const w = fs.createWriteStream(fileToWritePath);
+
+  stdin.pipe(w);
 };
 
 await write();
