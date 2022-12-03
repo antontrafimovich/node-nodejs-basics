221cadf248aa05763def1b66f506ca68448f93ef
Anton Trafimovich <antont@compatibl.com>
2022-12-03 21:24:47 +0100

    feat: add decompress.js implementation.

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import { createUnzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+import { fileURLToPath } from "node:url";
+import path from "path";
+
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const archivePath = path.resolve(__dirname, "files", "archive.gz");
+  const decompressedFile = path.resolve(
+    __dirname,
+    "files",
+    "fileToCompress1.txt"
+  );
+
+  const unzip = createUnzip();
+  const r = createReadStream(archivePath);
+  const w = createWriteStream(decompressedFile);
+
+  try {
+    await pipe(r, unzip, w);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
