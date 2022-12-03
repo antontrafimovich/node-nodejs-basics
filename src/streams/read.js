39cc84d64ee2620346792b20402cc8f1852eb56a
Anton Trafimovich <antont@compatibl.com>
2022-12-03 14:44:27 +0100

    feat: add stream read.js implementation.

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+
+import { fileURLToPath } from "node:url";
+import path from "path";
+import { stdout } from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToReadPath = path.resolve(__dirname, "files", "fileToRead.txt");
+
+  const r = fs.createReadStream(fileToReadPath);
+
+  r.pipe(stdout);
 };
 
 await read();
