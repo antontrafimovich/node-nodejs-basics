f226a5500d2639788a3f0f30cfef1acc9da29b87
Anton Trafimovich <antont@compatibl.com>
2022-12-03 13:44:17 +0100

    feat: add calcHash.js implementation.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import crypto from "node:crypto";
+import fs from "fs";
+
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToReadPath = path.resolve(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  try {
+    const content = await readFile(fileToReadPath, { encoding: "utf-8" });
+    const hashSum = crypto.createHash("sha256");
+    hashSum.update(content, "utf-8");
+    console.log(hashSum.digest("hex"));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await calculateHash();
