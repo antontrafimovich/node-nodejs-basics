fc91c2fe86b9439a417e2703f5c316553df4beac
Anton Trafimovich <antont@compatibl.com>
2022-12-03 21:10:40 +0100

    feat: add compress.js implementation.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+import { fileURLToPath } from "node:url";
+import path from "path";
+
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToCompressPath = path.resolve(
+    __dirname,
+    "files",
+    "fileToCompress.txt"
+  );
+  const archivePath = path.resolve(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const r = createReadStream(fileToCompressPath);
+  const w = createWriteStream(archivePath);
+  try {
+    await pipe(r.setEncoding("utf-8"), gzip, w);
+  } catch (error) {
+    console.error(error);
+  }
+
+  r.setEncoding("utf-8").pipe(gzip).pipe(w);
 };
 
 await compress();
