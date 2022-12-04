a395ebc625595b3b7e20eefe0e7b9e1107901597
Anton Trafimovich <antont@compatibl.com>
2022-12-04 23:19:54 +0100

    feat: add worker threads implementation

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "node:worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (result) => {
+  parentPort.postMessage(result);
+  // This function sends result of nthFibonacci computations to main thread
 };
 
-sendResult();
+parentPort.on("message", (n) => {
+  const result = nthFibonacci(n);
+
+  sendResult(result);
+});
+
+// sendResult();
