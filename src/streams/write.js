33c1bf5ee944c0384c810fd8ff292881d7c6ce66
Anton Trafimovich <antont@compatibl.com>
2022-12-05 19:23:38 +0100

    fix: added slight fix to streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 
 import { fileURLToPath } from "node:url";
-import path from "path";
+import path from "node:path";
 import { stdin } from "node:process";
 
 const write = async () => {
